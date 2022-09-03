89f1cffe187f9a4194ded851e2419b476c9d7817
desyercavallone <112729398+desyercavallone@users.noreply.github.com>
2022-09-03 23:33:39 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -70,7 +70,7 @@ global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Gli sticker del rabott " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "ℜタタ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
